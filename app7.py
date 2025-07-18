2612ebcfd1688f200bee7c8d666bc9ee370fc3dd
sebstr2358 <sebix2358@onet.pl>
2025-07-18 16:35:38 +0200

    change height of image

--- app7.py
+++ app7.py
@@ -290,7 +290,7 @@ if not st.session_state.get("openai_api_key"):
                 """
                 <div style="display: flex; flex-direction: column; justify-content: flex-start; height: 100%; height: 280px;">
                     <h1 style="text-align: center;">Zaloguj do OpenAI</h1>
-                    <div style="background-color: #3B4252; padding: 6px; border-radius: 5px; border: 1px solid #88C0D0; margin-top: 0; margin-bottom: 10px;">
+                    <div style="background-color: #3B4252; padding: 6px; padding-left: 8px; border-radius: 5px; border: 1px solid #88C0D0; margin-top: 0; margin-bottom: 10px;">
                         <h4 style="color: #F0F8FF;">Instrukcje uzyskania klucza API</h4>
                         <ol style="color: #F0F8FF;">
                             <li>Załóż konto na stronie <a href="https://platform.openai.com/signup" target="_blank" style="color: #88C0D0;">OpenAI</a>.</li>
